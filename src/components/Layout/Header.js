09dc80ff801678a2dd2a69d5887f9742d064ede1
Jingru Xu <jingruxu@Jingrus-Air.lan>
2023-03-06 23:45:41 -0500

    Add/Manage Cart and Modal Component

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = (props) => {
     <>
       <header className={classes.header}>
         <h1>Food Ordering App</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImg} alt="table of full of foods" />
